bedeaa2a9e957d09dbd69b8a89888423c4a7c41f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-10 22:32:43 +0100

    Add a reuseable button component

---
+++ src/ui/Button.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+interface ButtonProps
+    extends React.DetailedHTMLProps<
+        React.ButtonHTMLAttributes<HTMLButtonElement>,
+        HTMLButtonElement
+    > {
+    children: React.ReactNode;
+    url?: string;
+}
+
+const Button = ({ children, url = "" }: ButtonProps) => {
+    if (url) {
+        return (
+            <Link href={url} className="bg-secondary w-fit text-light px-4 py-2 rounded-md">
+                {children}
+            </Link>
+        );
+    }
+
+    return (
+        <button className="bg-secondary w-fit text-light px-4 py-2 rounded-md">{children}</button>
+    );
+};
+
+export default Button;
