71fa9cd5e701def3867183670eef07ec88ab0e30
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-10 22:29:44 +0100

    Add PageLayout to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-    return <main>Sesto Elemento</main>;
+import HomeMain from "@/components/HomeMain";
+import PageLayout from "@/components/PageLayout";
+import CoreValues from "@/components/CoreValues";
+import WhyUs from "@/components/WhyUs";
+import WhyUsFirstSlice from "@/components/WhyUsFirstSlice";
+import WhyUsSecondSlice from "@/components/WhyUsSecondSlice";
+
+export default function HomePage() {
+    return (
+        <PageLayout>
+            <HomeMain />
+            <CoreValues />
+            <WhyUs />
+            <WhyUsFirstSlice />
+            <WhyUsSecondSlice />
+        </PageLayout>
+    );
 }
