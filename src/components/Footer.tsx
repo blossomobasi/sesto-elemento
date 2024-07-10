455a78be262c1ae64667f093871bb18c94295c7c
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-10 22:30:14 +0100

    Create a footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+// import Image from "next/image";
+
+const Footer = () => {
+    return (
+        <footer>
+            Footer
+            {/* <div>
+                <div>
+                    <h2>Reach Out to Us</h2>
+                </div>
+                <Image
+                    src="/images/reach_us.png"
+                    alt="Reach out to us"
+                    width={1000}
+                    height={1000}
+                    className="w-[500px] h-[500px]"
+                />
+            </div>
+            <div></div> */}
+        </footer>
+    );
+};
+
+export default Footer;
