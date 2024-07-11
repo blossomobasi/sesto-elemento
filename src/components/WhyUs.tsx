cf2d77d7c7170d9989eebc2b6b9ef017897f24f2
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-11 20:46:53 +0100

    Style: Implement responsiveness for WhyUs component

--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -5,23 +5,25 @@ import Image from "next/image";
 const WhyUs = () => {
     return (
         <section className="bg-primaryVeryLight py-5">
-            <div className="gap-x-8 flex flex-col md:flex-row items-center justify-between">
+            <div className="gap-x-8 flex flex-col md:flex-row items-center justify-between gap-y-10 md:gap-y-0">
                 <Image
                     src="/images/why_us.png"
                     alt="Why Us"
-                    className="w-1/2"
+                    className="md:w-1/2"
                     width={1000}
                     height={1000}
                 />
-                <div className="flex flex-col items-start gap-y-5 mr-12 w-1/2">
-                    <h2 className="text-6xl font-semibold text-secondary">Why Us?</h2>
-                    <p className="text-2xl">
+                <div className="flex flex-col md:items-start gap-y-5 md:mr-12 mr-5 md:w-1/2 px-5 md:text-start text-center">
+                    <h2 className="lg:text-6xl text-4xl font-semibold text-secondary">Why Us?</h2>
+                    <p className="lg:text-2xl text-lg">
                         Our track record speaks for itself. We have a long history of successfully
                         executing projects of all sizes and complexities. Our satisfied clients have
                         consistently praised our ability to deliver projects on time, within budget,
                         and to the highest quality standards.
                     </p>
-                    <Button>Work with us</Button>
+                    <span>
+                        <Button>Work with us</Button>
+                    </span>
                 </div>
             </div>
         </section>
