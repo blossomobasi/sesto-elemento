98aaea4119ec35b2a83532d2dafcda5c3f7cd7e1
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-11 13:48:58 +0100

    Update Use flex for the responsiveness

--- src/components/CoreValues.tsx
+++ src/components/CoreValues.tsx
@@ -8,12 +8,24 @@ const CoreValues = () => {
     ];
 
     return (
-        <div className="px-12">
-            <h2 className="text-center text-5xl font-semibold text-secondary py-20">
+        <div className="px-12 py-28">
+            <h2 className="text-center text-5xl font-semibold text-secondary pb-28">
                 Our Core Values
             </h2>
 
-            <div className="grid grid-cols-5 gap-x-5 p-2">
+            <div className="flex flex-wrap justify-center gap-5">
+                {coreValues.map((value, index) => (
+                    <div
+                        key={index}
+                        className="bg-primaryLight rounded-md shadow-md py-5 px-4 flex flex-col gap-y-3 items-center justify-center w-52 h-52"
+                    >
+                        <div className="h-16 w-16 bg-primary rounded-full" />
+                        <p className="text-2xl text-center">{value}</p>
+                    </div>
+                ))}
+            </div>
+
+            {/* <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-x-5">
                 {coreValues.map((value) => (
                     <div
                         key={value}
@@ -23,7 +35,7 @@ const CoreValues = () => {
                         <p className="text-2xl text-center">{value}</p>
                     </div>
                 ))}
-            </div>
+            </div> */}
         </div>
     );
 };
