b40dcd2e10b97e332ebd2b0ac4bb73d5706c9daf
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-11 13:50:23 +0100

    Update: Add width specificly

--- src/components/WhyUsFirstSlice.tsx
+++ src/components/WhyUsFirstSlice.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const WhyUsFirstSlice = () => {
     return (
-        <div className="flex items-center justify-between gap-x-8">
-            <div className="pl-12 flex flex-col space-y-5">
-                <h2 className="text-4xl font-semibold">
+        <div className="flex items-center justify-between gap-x-8 py-5">
+            <div className="pl-12 flex flex-col items-start space-y-5 w-1/2">
+                <h2 className="text-5xl font-semibold">
                     <span className="text-primary">Maximizing</span>{" "}
                     <span className="text-secondary">Operational Efficiency for </span>
                     <span className="text-primary">Sustainable Growth</span>
@@ -18,10 +18,11 @@ const WhyUsFirstSlice = () => {
             </div>
 
             <Image
+                className="w-1/2"
                 src="/images/maximizing.png"
                 alt="Maximizing Operational Efficiency"
-                width={700}
-                height={700}
+                width={1000}
+                height={1000}
             />
         </div>
     );
