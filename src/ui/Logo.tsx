f19265b0bcdac58456356573c1f3f10e86113568
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-11 20:58:21 +0100

    Add link to logo

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
     return (
-        <span>
+        <Link href="/">
             <Image src="/images/logo.png" alt="Logo" width={167} height={63} />
-        </span>
+        </Link>
     );
 };
 
