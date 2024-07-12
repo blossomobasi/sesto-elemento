5573da8ad487d66f7e857c62c9ceefad3ee2a6d7
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 14:41:28 +0100

    Create Logo data

---
+++ src/data/logos.ts
@@ -0,0 +1,31 @@
+interface Logo {
+    alt: string;
+    src: string;
+}
+
+export const clientLogo: Logo[] = [
+    { alt: "Client 1", src: "client_1.png" },
+    { alt: "Client 2", src: "client_2.png" },
+    { alt: "Client 3", src: "client_3.png" },
+    { alt: "Client 4", src: "client_4.png" },
+    { alt: "Client 5", src: "client_5.png" },
+    { alt: "Client 6", src: "client_6.png" },
+    { alt: "Client 7", src: "client_7.png" },
+    { alt: "Client 8", src: "client_8.png" },
+    { alt: "Client 9", src: "client_9.png" },
+    { alt: "Client 10", src: "client_10.png" },
+    { alt: "Client 11", src: "client_11.png" },
+    { alt: "Client 12", src: "client_12.png" },
+    { alt: "Client 13", src: "client_13.png" },
+];
+
+export const partnersLogo: Logo[] = [
+    { alt: "partners 1", src: "partner_1.png" },
+    { alt: "partners 2", src: "partner_2.png" },
+    { alt: "partners 3", src: "partner_3.png" },
+    { alt: "partners 4", src: "partner_4.png" },
+    { alt: "partners 5", src: "partner_5.png" },
+    { alt: "partners 6", src: "partner_6.png" },
+    { alt: "partners 7", src: "partner_7.png" },
+    { alt: "partners 8", src: "partner_8.png" },
+];
