4ed71072b54025c6fc15e597afb0471cfc0ae73a
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 14:39:22 +0100

    Refactor: Extract clientLogo in data folder

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,21 +1,8 @@
+import { clientLogo as clientLogoApi } from "@/data/logos";
 import Image from "next/image";
 
 const Clients = () => {
-    const clientLogo = [
-        { alt: "Client 1", src: "client_1.png" },
-        { alt: "Client 2", src: "client_2.png" },
-        { alt: "Client 3", src: "client_3.png" },
-        { alt: "Client 4", src: "client_4.png" },
-        { alt: "Client 5", src: "client_5.png" },
-        { alt: "Client 6", src: "client_6.png" },
-        { alt: "Client 7", src: "client_7.png" },
-        { alt: "Client 8", src: "client_8.png" },
-        { alt: "Client 9", src: "client_9.png" },
-        { alt: "Client 10", src: "client_10.png" },
-        { alt: "Client 11", src: "client_11.png" },
-        { alt: "Client 12", src: "client_12.png" },
-        { alt: "Client 13", src: "client_13.png" },
-    ];
+    const clientLogo = clientLogoApi;
 
     // const logos = clientLogo.map((client) => client.src);
 
