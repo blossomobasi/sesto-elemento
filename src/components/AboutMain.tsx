fc75c2d683d446dd97d1ed8662646df29433b622
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 13:19:14 +0100

    Implement about main component

---
+++ src/components/AboutMain.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const AboutMain = () => {
+    return (
+        <section className="bg-primaryVeryLight h-[calc(100vh-7rem)] flex flex-col items-center justify-center text-center space-y-5 px-16">
+            <h1 className="text-6xl font-extrabold font-lato">
+                <span className="text-secondary">SESTO </span>
+                <span className="text-primary">ELEMENTO </span> <br />
+                <span className="text-primary">SERVICES </span>
+                <span className="text-secondary">LIMITED</span>
+            </h1>
+
+            <div className="h-5 w-5 rounded-full bg-secondary absolute top-32 left-20" />
+            <div className="h-5 w-5 rounded-full bg-secondary absolute top-48 left-[26rem]" />
+            <div className="h-5 w-5 rounded-full bg-secondary absolute top-56 right-56" />
+
+            <div className="h-5 w-5 rounded-full bg-primary absolute left-32 top-[22rem]" />
+            <div className="h-5 w-5 rounded-full bg-primary absolute left-[45rem] bottom-20" />
+            <div className="h-5 w-5 rounded-full bg-primary absolute right-20 bottom-32" />
+
+            <div>
+                <p>
+                    Sesto elemento services Limited was established in 2013, a leading, Procurement,
+                    Integration, Installation and Maintenance provider.
+                </p>
+                <br />
+                <p>
+                    Sesto elemento is headquartered in Lagos Nigeria and has a satellite office in
+                    Port Harcourt and Abuja. Our primary focus is on Project Procurement services,
+                    Technical site assistance services innovative solutions enhancing Production
+                    (Oil, Gas, Minerals), Mining, Pipeline Infrastructure, while its scope extends
+                    to various environmentally responsible technologies enhance clean and renewable
+                    energy.
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMain;
