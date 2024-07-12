5f48d089d603385a9d5fc6d2ade8e9aaa221c8f7
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 07:08:07 +0100

    create footer data

---
+++ src/data/footerData.ts
@@ -0,0 +1,23 @@
+import { subLink } from "@/types/nav";
+
+export const companyData: subLink[] = [
+    { title: "About Us", href: "/about" },
+    { title: "our Partners", href: "/partners" },
+    { title: "Faq", href: "/" },
+];
+
+export const services: subLink[] = [
+    { title: "Procurement", href: "/sevices/#procurement" },
+    { title: "Contracting", href: "/sevices/#contraction" },
+    { title: "Man Power Supply", href: "/sevices/#man-power-supply" },
+    { title: "Technical Site Asst", href: "/sevices/#technical-site-asst" },
+    { title: "Equipment Lease", href: "/sevices/#equipment-lease" },
+];
+
+export const sectors: subLink[] = [
+    { title: "Oil and Gas", href: "/sector/#oil-and-gas" },
+    { title: "Defence", href: "/sector/#defence" },
+    { title: "Power Generation", href: "/sector/#power-generation" },
+    { title: "Infrastructure", href: "/sector/#infrastructure" },
+    { title: "Renewable", href: "/sector/#renewable" },
+];
