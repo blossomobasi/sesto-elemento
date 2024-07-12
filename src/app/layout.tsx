9ce8a105dd13bcef33e7d5d6fd4cc233f454cc07
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 13:18:20 +0100

    Add lato font as a variable in body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
 const lexend = Lexend({ subsets: ["latin"] });
+
+const lato = Lato({
+    variable: "--font-lato",
+    subsets: ["latin"],
+    weight: ["900"],
+});
 
 export const metadata: Metadata = {
     title: "Sesto Elemento",
@@ -16,7 +23,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${lexend.className} bg-light overflow-x-hidden text-textColor`}>
+            <body
+                className={`${lato.variable} ${lexend.className} bg-light overflow-x-hidden text-textColor`}
+            >
                 {children}
             </body>
         </html>
