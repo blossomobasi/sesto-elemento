6432e74d71455a703c9393da2bfb20ab49220458
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 14:38:33 +0100

    Create a partners page

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,14 @@
+import Clients from "@/components/Clients";
+import PageLayout from "@/components/PageLayout";
+import Partners from "@/components/Partners";
+
+const PartnersPage = () => {
+    return (
+        <PageLayout>
+            <Partners />
+            <Clients />
+        </PageLayout>
+    );
+};
+
+export default PartnersPage;
