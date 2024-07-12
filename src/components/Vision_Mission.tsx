4aa59907d1f2c71f75f776fbae90b7807dc8bbbb
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 14:40:49 +0100

    Responsive: Add responsive feature for Vision_Mission component

--- src/components/Vision_Mission.tsx
+++ src/components/Vision_Mission.tsx
@@ -2,26 +2,26 @@ import Image from "next/image";
 
 const Vision_Mission = () => {
     return (
-        <section className="py-14 md:px-16 px-5">
-            <div className="text-center">
-                <h2 className="text-5xl font-semibold text-secondary">Who we are</h2>
-                <p className="text-lg mt-5 md:px-20 px-5">
+        <section className="pt-10 md:px-16 px-5">
+            <div className="text-center pb-10">
+                <h2 className="md:text-5xl text-4xl font-semibold text-secondary">Who we are</h2>
+                <p className="md:text-lg mt-5 md:px-20 px-5">
                     We are a projects and operations support services firm poised towards the
                     provision of solutions in the Oil, Energy, Marine, Building and Civil
                     Infrastructure Industry where we develop unrivaled trailblazing solutions in
                     building sustainable infrastructures that comply with global best practice.
                 </p>
             </div>
-            <div className="flex items-center ">
+            <div className="flex items-center lg:flex-row flex-col">
                 <Image
                     src="/images/who_we_are.png"
                     alt="Who we are"
                     width={1000}
                     height={1000}
-                    className="w-1/2"
+                    className="lg:w-1/2"
                 />
 
-                <div className="w-1/2 flex flex-col space-y-5">
+                <div className="lg:w-1/2 flex flex-col space-y-5 md:text-left text-center">
                     <div className="flex flex-col space-y-3">
                         <h3 className="text-secondary text-3xl font-semibold">Our vision</h3>
                         <p className="text-lg">
