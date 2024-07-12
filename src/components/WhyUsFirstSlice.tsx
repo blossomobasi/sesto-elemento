0a9de1bc7c5dc98847346b149b4a274ec17b6b7b
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 07:07:34 +0100

    style: increase x-axis padding

--- src/components/WhyUsFirstSlice.tsx
+++ src/components/WhyUsFirstSlice.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const WhyUsFirstSlice = () => {
     return (
         <div className="flex items-center justify-between flex-col-reverse md:flex-row gap-x-8 gap-y-10 md:gap-y-0 py-5">
-            <div className="md:pl-12 px-5 flex flex-col md:items-start items-center space-y-5 md:w-1/2 md:text-start text-center">
+            <div className="md:pl-16 px-5 flex flex-col md:items-start items-center space-y-5 md:w-1/2 md:text-start text-center">
                 <h2 className="lg:text-5xl md:text-4xl text-3xl font-semibold">
                     <span className="text-primary">Maximizing </span>
                     <span className="text-secondary">Operational Efficiency for </span>
