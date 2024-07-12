8daaaaf194f8790753e630a5758b910783cf3060
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-12 14:39:57 +0100

    Update: Add responsiveness for OurMotivation component

--- src/components/OurMotivation.tsx
+++ src/components/OurMotivation.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 
 const OurMotivation = () => {
     return (
-        <section className="flex items-center justify-between md:px-16 px-5">
-            <div className="flex flex-col space-y-5 md:items-start items-center md:text-start text-center">
-                <h2 className="text-6xl text-secondary font-semibold">Our Motivation</h2>
+        <section className="flex items-center justify-between md:flex-row flex-col-reverse space-x-5 md:px-16 px-5 py-10">
+            <div className="flex flex-col space-y-5 md:items-start items-center md:text-start text-center md:w-1/2">
+                <h2 className="lg:text-6xl text-4xl text-secondary font-semibold">
+                    Our Motivation
+                </h2>
                 <p>
                     We are motivated to achieve the elements needed to provide project success and
                     build structures and roadways that will leave a lasting impression for our
@@ -19,6 +21,7 @@ const OurMotivation = () => {
                 alt="Our Motivation"
                 width={1000}
                 height={1000}
+                className="md:w-1/2"
             />
         </section>
     );
