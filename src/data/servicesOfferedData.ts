ec3cf29073caa022c4ea24cc899dab4a987dd586
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-13 15:29:03 +0100

    Create servicesOffered data in a seperate file

---
+++ src/data/servicesOfferedData.ts
@@ -0,0 +1,82 @@
+export const servicesOffered = [
+    {
+        title: "Procurement",
+        firstParagraph:
+            "We provide equipment, materials and consumable related to oil sectors, marine and power generation and renewable energy industry applications.",
+        image: {
+            src: "/images/procurement.png",
+            alt: "Procurement",
+        },
+    },
+    {
+        title: "Contracting",
+        firstParagraph:
+            "We understand the critical importance of seamless project execution and operational efficiency. With our expertise and unwavering commitment, we provide comprehensive contracting solutions and unparalleled support to help your business thrive.",
+        secondParagraph:
+            "Our team of seasoned professionals possesses extensive industry knowledge and a proven track record in managing diverse projects across various sectors. From inception to completion, we work closely with you to ensure that your project is delivered on time, within budget, and to the highest quality standards.",
+        image: {
+            src: "/images/contracting.png",
+            alt: "Contracting",
+        },
+    },
+    {
+        title: "Marine",
+        firstParagraph:
+            "Specialized OSVs DP2 Subsea Support Vessels DP2 AHTS DP2 PSVs DP2 Diving Support Vessels Seismic Support Vessels SESTO ELEMENTO SERVICES introduces its marine service support division, We providefit-for-purposeassets designed with specific clients in mind, in consultation with them,focusingon existing and anticipated needs in fourverticals whichinclude Specialized OSVs, General Purpose OSVs, Crew boats, Near Shore Vessels, Barges & Security Vessels. Our diverse fleet can service our clients across all phases of the oil and gas cycle. General Purpose OSVs AHTS / AHTs PSVs Accommodation Work Barges & Workboats Geotechnical Vessels Multi Purpose Offshore Terminal Tugs MPSVs Utility Vessels   Crew boats Near Shore Vessels Barges & Security Vessels Marine Equipment Chartering Commercial Management of Barges and Tow/Support Tugs (40 to 110T  BP)",
+        secondParagraph: "Project Cargo and Pipe Transportation",
+        listItem: [
+            "Turn key or part lump sum/ part time charter ",
+            "Tailor-made marine project cargo transportationsolutions ",
+            "Economical and efficient transport solutions ",
+            "Special Transports, e.g. Pipe, Modules, Xblocks,piles, deck, jackets, structures, etc.",
+            "Marine Equipment Solutions ",
+            "Charter of (modified) equipment for specificproject requirements",
+            "Provision of complete Marine Equipment Packagesfor offshore and near shore projects ",
+            "Vessel Chandlering Services, Supplies of Fuels and Lubricants.",
+        ],
+        image: {
+            src: "/images/marine.png",
+            alt: "Marine",
+        },
+    },
+    {
+        title: "Man power supply",
+        firstParagraph:
+            "We recognize that skilled and efficient manpower is the backbone of any successful project. With our expertise in sourcing and providing top-tier talent, we ensure that your workforce is equipped to meet the demands of your project and drive optimal operational performance.",
+        image: {
+            src: "/images/man_power.png",
+            alt: "Man power supply",
+        },
+    },
+    {
+        title: "Equipment Lease",
+        firstParagraph:
+            "We are aware that having the right equipment is essential for the seamless execution of your projects. We offer a comprehensive range of equipment lease options tailored to meet your specific needs, ensuring that you have access to the tools necessary to drive efficiency and productivity.",
+        secondParagraph:
+            "Our extensive fleet of state-of-the-art equipment is meticulously maintained and regularly serviced to deliver exceptional performance and reliability. From heavy machinery to specialized tools, we have the resources to support a wide range of industries and project requirements.",
+        image: {
+            src: "/images/equipment_lease.png",
+            alt: "Equipment Lease",
+        },
+    },
+    {
+        title: "Technical Site Asst",
+        firstParagraph:
+            "All Customers are invited to benefit from our customer technical assistance either onsite or offsite in order to provide better services. Responding to domestic and international customers’ inquiries in a short time, and accurate transfer of customers’ demand to the related department. Customers can benefit from technical services in 3 main categories: Before Sales, During sales, After Sales.",
+        secondParagraph: "We provide the following services:",
+        listItem: [
+            "Inspection",
+            "Installation – Electrical and Instrument",
+            "Equipment Commissioning & Decommissioning",
+            "Various Surveys Services",
+            "Supply and erection of scaffolding",
+            "Marine Services",
+            "Equipment rental",
+            "Permanent and Temporary site accommodation",
+        ],
+        image: {
+            src: "/images/technical_site.png",
+            alt: "Technical Site Asst",
+        },
+    },
+];
