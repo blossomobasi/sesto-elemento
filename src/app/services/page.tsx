bc7ec092697cc6a32ee87db623353c5e8c3c416f
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-13 15:27:48 +0100

    Add Sevices page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,14 @@
+import PageLayout from "@/components/PageLayout";
+import ServiceMain from "@/components/ServiceMain";
+import ServiceOffer from "@/components/ServiceOffer";
+
+const Services = () => {
+    return (
+        <PageLayout>
+            <ServiceMain />
+            <ServiceOffer />
+        </PageLayout>
+    );
+};
+
+export default Services;
