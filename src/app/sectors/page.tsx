d33f17d7c77131e6580184d3ea1cd624e6116722
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-14 15:19:20 +0100

    Add Sectors Page

---
+++ src/app/sectors/page.tsx
@@ -0,0 +1,14 @@
+import PageLayout from "@/components/PageLayout";
+import Sectors from "@/components/Sectors";
+import WhyUsSecondSlice from "@/components/WhyUsSecondSlice";
+
+const SectorsPage = () => {
+    return (
+        <PageLayout>
+            <WhyUsSecondSlice />
+            <Sectors />
+        </PageLayout>
+    );
+};
+
+export default SectorsPage;
