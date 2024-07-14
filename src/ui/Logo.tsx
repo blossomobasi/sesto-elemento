5bbff21b80fa789591cdf7da9378f0f39956272d
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-14 13:26:12 +0100

    Add responsive size for Logo

--- src/ui/Logo.tsx
+++ src/ui/Logo.tsx
@@ -4,7 +4,13 @@ import Link from "next/link";
 const Logo = () => {
     return (
         <Link href="/">
-            <Image src="/images/logo.png" alt="Logo" width={167} height={63} />
+            <Image
+                src="/images/logo.png"
+                alt="Logo"
+                width={167}
+                height={63}
+                className="lg:scale-100 md:scale-75 scale-0"
+            />
         </Link>
     );
 };
