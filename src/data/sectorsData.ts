6e78cf06bcd8e62531fb667448bf46a243841fdf
Obasi Chizitere <blossomobasi2@gmail.com>
2024-07-14 15:21:29 +0100

    Implement sectors data

---
+++ src/data/sectorsData.ts
@@ -0,0 +1,51 @@
+export const sectorsData = [
+    {
+        title: "Oil, Gas, and Energy",
+        firstParagraph:
+            "Our team is experienced in supporting projects in the oil, gas, and energy sectors, including exploration, production, refining, and distribution. We offer specialized services such as manpower supply, equipment lease, logistics management, and safety compliance.",
+        image: {
+            src: "/images/oil_and_gas.png",
+            alt: "Oil and Gas",
+        },
+    },
+    {
+        title: "Construction and Contracting",
+        firstParagraph:
+            " We provide end-to-end support for construction and engineering projects, offering services such as construction management, design coordination, procurement, and skilled labor supply.",
+        image: {
+            src: "/images/construction_and_engineering.png",
+            alt: "Construction and Engineering",
+        },
+    },
+    {
+        title: "Defence",
+        firstParagraph:
+            "SESTO ELEMENTO SERVICES supply a broad portfolio of airborne, land and naval systems and products for defense, homeland security and commercial applications. Our systems and products are installed on new platforms, and we also perform comprehensive platform modernization programs. In addition, we provide a range of training and support services. Our major activities include: military aircraft and helicopter systems; commercial aviation systems and aerostructures; unmanned aircraft systems (UAS); electro-optic, night vision and countermeasures systems; naval systems; land vehicle systems; munitions; command, control, communications, computer, intelligence, surveillance and reconnaissance (C4ISR) and cyber systems; electronic warfare and signal intelligence systems; and other commercial activities. Many of these major activities have a number of common and related elements, including common technologies and products, types of programs and customer interface.",
+        secondParagraph:
+            "Therefore, certain of our subsidiaries, divisions or other operating units often jointly conduct marketing, research and development, manufacturing, performance of programs, sales and after sales support among these major activities.",
+        image: {
+            src: "/images/defence.png",
+            alt: "Defence",
+        },
+    },
+    {
+        title: "Infrastructure",
+        firstParagraph:
+            "From transportation networks to public facilities, we provide comprehensive support for infrastructure development projects. Our services include project planning, contract management, stakeholder coordination, and quality assurance.",
+        image: {
+            src: "/images/infrastructure.png",
+            alt: "Infrastructure",
+        },
+    },
+    {
+        title: "Renewable",
+        firstParagraph:
+            "From project inception to ongoing operations, we offer tailored solutions to support your renewable energy endeavors. Our services include project planning, feasibility studies, permitting and regulatory compliance, equipment procurement, construction management, and maintenance support.",
+        secondParagraph:
+            "We also have extensive experience in optimizing renewable energy systems, ensuring maximum efficiency and performance. Our team can assist with energy storage solutions, grid integration, monitoring and control systems, and ongoing operations and maintenance.",
+        image: {
+            src: "/images/renewable.png",
+            alt: "Renewable",
+        },
+    },
+];
